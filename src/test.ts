89436a4774bbbfa79c5f90789b634c3df3acea7f
jimao <jimao@jimaodeMacBook-Pro.local>
2022-02-09 10:56:11 +0800

    提交

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 
 import { Interval } from './index';
 
-const i = new Interval('1m');
+const i = new Interval('1d');
 setInterval(() => {
-  console.log(i.distance() / 1000);
+  console.log(i.distance() / 1000 / 60 / 60);
 }, 1000);
